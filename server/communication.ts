cc9475d712dc633167cd3cdc95a583048566e318
lysmac <calle@lysmask.se>
2023-05-04 10:38:39 +0200

    changed the interfaces a bit

--- server/communication.ts
+++ server/communication.ts
@@ -4,6 +4,7 @@ export interface ServerToClientEvents {
 
 export interface ClientToServerEvents {
   message(message: string): void;
+  history: (messages: string[]) => void;
   join?: (room: string) => void;
   leave?: (room: string) => void;
 }
@@ -16,13 +17,3 @@ export interface SocketData {
   name: string;
   age: number;
 }
-
-// wrong, fix this
-export type ServerClientToServerEvents = {
-  message: (message: string) => void;
-};
-
-export type ServerServerToClientEvents = {
-  history: (messages: string[]) => void;
-  message: (message: string) => void;
-};
