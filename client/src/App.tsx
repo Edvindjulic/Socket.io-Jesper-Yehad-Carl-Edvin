14519e4814a302f3af5587b6b754315a25e4f739
lysmac <calle@lysmask.se>
2023-05-04 10:34:26 +0200

    context and main/app added from earlier example

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,1 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { useSocket } from "./context/SocketContext";
 
+function App() {
+  const { socket } = useSocket();
+  const [messages, setMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    if (!socket) return;
+
+    const handleHistory = (history: string[]) => {
+      setMessages(history);
+    };
+
+    const handleMessage = (message: string) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    };
+
+    socket.on("history", handleHistory);
+    socket.on("message", handleMessage);
+
+    return () => {
+      socket.off("history", handleHistory);
+      socket.off("message", handleMessage);
+    };
+  }, [socket]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const messageInput = event.currentTarget.elements.namedItem(
+      "message"
+    ) as HTMLInputElement;
+    const message = messageInput.value;
+    socket.emit("message", message);
+    console.log("Sent message:", message);
+    messageInput.value = "";
+  };
+
+  return (
+    <>
+      <div>Chat?</div>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="message" />
+        <button type="submit">Send</button>
+      </form>
+    </>
+  );
+}
+
+export default App;
