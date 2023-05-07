710e6321ff6889d9377c6c45af7f201301be882e
edvindjulic <djulicedvin@gmail.com>
2023-05-07 23:23:44 +0200

    CSS + auto scroll to latest message. Maybe change to option to scroll down instead of auto?

--- client/src/components/ChatBox.tsx
+++ client/src/components/ChatBox.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, TextField, Typography, List, ListItem } from "@mui/material";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSocket } from "../context/SocketContext";
 
 const drawerWidth = "20vw";
@@ -7,12 +7,19 @@ const drawerWidth = "20vw";
 export default function ChatBox() {
   const [message, setMessage] = useState("");
   const { sendMessage, messages, currentRoom } = useSocket();
+  const latestMessageRef = useRef<HTMLLIElement>(null);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     sendMessage(message);
     setMessage("");
   };
+
+  useEffect(() => {
+    if (latestMessageRef.current) {
+      latestMessageRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [messages]);
 
   return (
     <Box
@@ -21,18 +28,39 @@ export default function ChatBox() {
         flexDirection: "column",
         alignItems: "center",
         marginLeft: drawerWidth,
-        backgroundColor: "lightgreen"
+        backgroundColor: "lightgreen",
       }}
     >
       <Typography variant="h6">Chat in {currentRoom} room</Typography>
-      <List>
-        {messages.map((message, i) => (
-          <ListItem key={i}>
-            {message.name}: {message.message}
-          </ListItem>
-        ))}
-      </List>
-      <Box component="form" onSubmit={handleSubmit}>
+      <Box
+        sx={{
+          width: "60vw",
+          height: "80vh",
+          backgroundColor: "lightblue",
+          marginBottom: "1rem",
+          overflowY: "auto",
+        }}
+      >
+        <List>
+          {messages.map((message, i) => (
+            <ListItem key={i} ref={i === messages.length - 1 ? latestMessageRef : null}>
+              {message.name}: {message.message}
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          marginBottom: "1rem",
+        }}
+      >
         <TextField
           name="message"
           label="Write a message..."
@@ -41,8 +69,18 @@ export default function ChatBox() {
           onChange={(e) => setMessage(e.target.value)}
           variant="outlined"
           size="small"
+          sx={{
+            width: "50%",
+          }}
         />
-        <Button type="submit" variant="contained" color="primary">
+        <Button
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            width: "10%",
+          }}
+        >
           Send
         </Button>
       </Box>
