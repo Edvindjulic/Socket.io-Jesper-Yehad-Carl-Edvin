ea4f46bf447e5a0ff5d643d269fd7cbd8e3c2a3c
ye-mou <git.ymo22@gmail.com>
2023-05-11 11:31:48 +0200

    Hiding online users

--- client/src/components/Users.tsx
+++ client/src/components/Users.tsx
@@ -1,33 +1,46 @@
-import { List, ListItem, ListItemText, Typography, ListItemIcon } from "@mui/material";
+import CircleTwoToneIcon from "@mui/icons-material/CircleTwoTone";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useSocket } from "../context/SocketContext";
-import CircleTwoToneIcon from '@mui/icons-material/CircleTwoTone';
 
 export default function Users() {
   const { users } = useSocket();
 
   return (
-    <>
-      <Typography
-        variant="h6"
-        sx={{
-          textAlign: "center",
-        }}
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
       >
-        Online Users ({users.length})
-      </Typography>
-      <List>
-        {users.map((user) => (
-          <ListItem key={user.userID} sx={{py: 0}}>
-            <ListItemIcon>
-              <CircleTwoToneIcon fontSize="small" sx={{ color: '#57B49F' }} />
-            </ListItemIcon>
-            <ListItemText
-              primary={user.username}
-              secondary={`Room: ${user.room}`}
-            />
-          </ListItem>
-        ))}
-      </List>
-    </>
+        <Typography variant="h6" sx={{ textAlign: "center" }}>
+          Online Users ({users.length})
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <List>
+          {users.map((user) => (
+            <ListItem key={user.userID} sx={{ py: 0 }}>
+              <ListItemIcon>
+                <CircleTwoToneIcon fontSize="small" sx={{ color: "#57B49F" }} />
+              </ListItemIcon>
+              <ListItemText
+                primary={user.username}
+                secondary={`Room: ${user.room}`}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </AccordionDetails>
+    </Accordion>
   );
 }
